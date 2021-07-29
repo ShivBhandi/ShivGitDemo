69b9a3b10e06fc94f212e604bc352d2656f40a6c
Avi Kalyan <avi9730155543@gmail.com>
2021-07-29 17:45:40 +0530

    Commit full project

---
+++ src/main/java/com/qa/pages/SearchPage.java
@@ -0,0 +1,15 @@
+package com.qa.pages;
+
+public class SearchPage {
+
+	public void searchPage(){
+		
+		System.out.println("Search Page");
+	}
+	
+	public void searchBox(){
+		
+		System.out.println("Search Box");
+	}
+
+}
